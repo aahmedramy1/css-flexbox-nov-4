8407405f6e3d608627bdcaf3fb2fd66f3378fc8f
Ahmed Ramy <ahmedramy.e404@gmail.com>
2022-11-05 08:52:31 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,21 @@
+function fizzBuzz(n)
+{
+    var answer = [];
+    for (var i = 1; i <= n; i++)
+    {
+        if (i % 3 === 0 && i % 5 === 0) {
+            answer.push("FizzBuzz");
+        } else if (i % 5 === 0)
+        {
+            answer.push("Buzz");
+        } else if (i % 3 === 0)
+        {
+            answer.push("Fizz");
+        } else {
+            answer.push(i.toString());
+        }
+    }
+    return answer;
+}
+
+console.log(fizzBuzz(15));
